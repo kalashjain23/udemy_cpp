c39332bbee92605c48591189b1a092626dc6f7b3
Kalash <kalashjain513@gmail.com>
2023-01-07 23:22:10 +0530

    converted the basic cpp code to OOP style code

--- src/basic_cpp_node.cpp
+++ src/basic_cpp_node.cpp
@@ -1,10 +1,33 @@
 #include "rclcpp/rclcpp.hpp"
 
+class BasicNode : public rclcpp::Node
+{
+public:
+    BasicNode(): Node("basic_cpp"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello from cpp");
+
+        timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                        std::bind(&BasicNode::timer_callback, this));
+    }
+
+private:
+
+    void timer_callback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+
+};
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("basic_cpp");
-    RCLCPP_INFO(node->get_logger(), "Hello from cpp");
+    auto node = std::make_shared<BasicNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
